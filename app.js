5593888f69f676e5462d356f4e3dc834a85185eb
Serhii1727 <kravchenko087@ukr.net>
2024-04-13 13:11:24 -0400

    add file(start version) swagger

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import contactsRouter from './routes/contactsRouter.js';
-import dotenv from 'dotenv';
-import userRouter from './routes/auth.js';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+import contactsRouter from "./routes/contactsRouter.js";
+import dotenv from "dotenv";
+import userRouter from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
@@ -12,29 +14,34 @@ const app = express();
 mongoose
   .connect(process.env.CONNECTION_STRING)
   .then(() => {
-    console.log('Database connection successful');
+    console.log("Database connection successful");
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
-app.use('/api/users', userRouter);
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", userRouter);
+app.use(
+  "/api-docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
 app.listen(3000, () => {
-  console.log('Server is running. Use our API on port: 3000');
+  console.log("Server is running. Use our API on port: 3000");
 });
