3596ab3f0092dd43a0189e41b14467a854f3956d
Serhii1727 <kravchenko087@ukr.net>
2024-04-20 11:56:46 -0400

    change

--- services/waterNotesServices.js
+++ services/waterNotesServices.js
@@ -4,11 +4,11 @@ const createNewWaterNote = (data) => WaterNote.create({ ...data });
 
 const getOneWaterNote = (filter) => WaterNote.findOne(filter);
 
-const getOneAndUpdate = (filter, update, options) =>
-  WaterNote.findOneAndUpdate(filter, update, options);
+// const getOneAndUpdate = (filter, update, options) =>
+//   WaterNote.findOneAndUpdate(filter, update, options);
 
 export default {
   createNewWaterNote,
   getOneWaterNote,
-  getOneAndUpdate,
+  //getOneAndUpdate,
 };
