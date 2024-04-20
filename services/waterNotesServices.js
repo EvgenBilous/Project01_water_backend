f92bd48c202d5140d3f58583cc100342622aa55a
Serhii1727 <kravchenko087@ukr.net>
2024-04-19 21:50:17 -0400

    add update, delete. today

--- services/waterNotesServices.js
+++ services/waterNotesServices.js
@@ -4,7 +4,11 @@ const createNewWaterNote = (data) => WaterNote.create({ ...data });
 
 const getOneWaterNote = (filter) => WaterNote.findOne(filter);
 
+const getOneAndUpdate = (filter, update, options) =>
+  WaterNote.findOneAndUpdate(filter, update, options);
+
 export default {
   createNewWaterNote,
   getOneWaterNote,
+  getOneAndUpdate,
 };
